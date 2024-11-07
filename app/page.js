a3ab85a93199b23a31c7ab81e823d5f2e5e7d873
Samtrent17 <xamsamuel079@gmail.com>
2024-11-07 09:31:10 +0300

    initiate

--- app/page.js
+++ app/page.js
@@ -1,12 +1,18 @@
-import Title from "../src/components/Title"
+import Title from "../src/components/Title";
+
 import Contents from "../src/components/contents";
+
+import  Button  from "@mui/material/Button";
 
 function Home() {
 
   return (
   <div>
+
+
+
     <Title heading="DIME DRIP" subheading="Dime Drip is a very nice website"/>
-    <Contents details="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"/>
+    <Contents details="Dime Drip is a very helpful website"/>
 
 
     <br/>
@@ -14,7 +20,7 @@ function Home() {
 
 
     <Title heading="UCU" subheading="UCU is a very nice website"/>
-    <Contents details="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"/>
+    <Contents details="UCU is a very good University."/>
 
 
 
@@ -28,6 +34,12 @@ function Home() {
 
 
     <br/>
+
+    <Button variant="text" >Proceed</Button>
+    <Button variant="contained" disabled >Proceed</Button>
+    <Button variant="outlined" >Proceed</Button>
+
+
   </div>
   );
 };
